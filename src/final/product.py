38651be15bf2338332a738fa2e10e48e894636c5
JonathanWillnow <jona.willnow@hotmail.de>
2022-02-17 12:10:35 +0100

    Implementing weighting of the factors according to their long-term performance as recorded by Fama French on their website.

--- src/final/product.py
+++ src/final/product.py
@@ -11,40 +11,51 @@ from bs4 import BeautifulSoup
 from scipy import stats #The SciPy stats module
 import math #The Python math module
 
+def get_european_weights_ken_french():
+    from pandas_datareader.famafrench import get_available_datasets
+    import pandas_datareader.data as web
 
-rv_dataframe = pd.read_csv(r"frameResults.csv")
+    # default factor allocation due to past 6 months performance??!!
+    # -14 since factors come available with 2 months delay
+    # s_START_DATE = str(datetime.today().strftime("%Y-%m-%d").AddMonths(-8))
+    # s_END_DATE = str(datetime.today().strftime("%Y-%m-%d"))
 
+    START_DATE = '2000-05-01'
+    END_DATE = '2022-01-22'
 
-def weighted_average_m1(distribution, weights):
-  
-    numerator = sum([distribution[i]*weights[i] for i in range(len(distribution))])
-    denominator = sum(weights)
-    
-    return round(numerator/denominator,2)
-
-
-from statistics import mean
-metrics = {
-        'priceToBook':'PB_percentile',
-        'enterpriseValue': 'EV_percentile',
-        'priceToSalesTrailing12Months' : 'PS_percentile'
-        }
-weights = [100,1,1]
-for row in rv_dataframe.index:
-    value_percentiles = []
-    for i, metric in enumerate(metrics.keys()):
-        value_percentiles.append(rv_dataframe.loc[row, metrics[metric]] * weights[i])
-        #sum([distribution[i]*weights[i] for i in range(len(distribution))]) list comprehension
-    rv_dataframe.loc[row, 'RV Score'] = sum(value_percentiles) / sum(weights)
-    
-rv_dataframe
+    ff_dict = web.DataReader('Europe_5_Factors', 'famafrench', start=START_DATE, end = END_DATE) # default is monthly  #ff_dict.keys()  #print(ff_dict["DESCR"])
+    factor_df = ff_dict[0]#monthly factors. 1 for annual
+    factor_df.rename(columns = {"Mkt-RF": "mkt_rf", "SMB":"smb", "HML":"hml", "RMW":"rmw", "CMA":"cma", "RF":"rf"}, inplace= True) 
+    factor_df["mkt"] = factor_df.mkt_rf + factor_df.rf
+    factor_df = factor_df.apply(pd.to_numeric, errors='coerce').div(100)
+    #meandf = factor_df[["smb", "hml", "rmw", "cma"]].mean()*100
+    adjust_weights = factor_df[["smb", "hml", "rmw", "cma"]].mean()*100
+    return(adjust_weights.values)
 
 
-sorted_rc_frame = rv_dataframe.sort_values(by = 'RV Score').copy()
-sorted_rc_frame  = sorted_rc_frame [:25]
-sorted_rc_frame.reset_index(drop = True, inplace = True)
+def calculate_precentiles(rv_dataframe):
+    from statistics import mean
+    metrics = {
+            'enterpriseValue': 'EV_percentile',
+            'priceToSalesTrailing12Months' : 'PS_percentile',
+            }
 
-position_size = float(1_000_000) / len(sorted_rc_frame.index)
-for i in range(0, len(sorted_rc_frame['Unnamed: 0'])-1):
-    sorted_rc_frame.loc[i, 'Number of Shares to Buy'] = math.floor(position_size / sorted_rc_frame['currentPrice'][i])
-sorted_rc_frame
+
+    FF_metrics = {
+            'marketCap': 'MC_percentile',
+            'priceToBook':'PB_percentile',
+            'FF_Quality_mean' : 'FFQ(inv)_m_percentile',
+            #'FF_Quality_Growth' : 'FFQ(inv)_g_percentile',
+            'FF_Assets_Growth_mean': 'FFA_m_percentile',
+    }
+    weights = [100,1,1]
+    ff_weights = get_european_weights_ken_french()
+    print(ff_weights)
+    for row in rv_dataframe.index:
+        value_percentiles = []
+        for i, metric in enumerate(FF_metrics.keys()):
+            value_percentiles.append(rv_dataframe.loc[row, FF_metrics[metric]] * ff_weights[i])
+        rv_dataframe.loc[row, 'RV Score'] = sum(value_percentiles) / sum(ff_weights)
+        
+    return rv_dataframe
+
