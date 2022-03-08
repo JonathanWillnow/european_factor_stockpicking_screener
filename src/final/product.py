c664618b887794a995c074064a20ae082e23bb01
JonathanWillnow <jona.willnow@hotmail.de>
2022-03-08 15:00:14 +0100

    Reformatting all ode in /final.

--- src/final/product.py
+++ src/final/product.py
@@ -6,9 +6,10 @@ import numpy as np
 import pandas as pd
 from pandas import ExcelWriter
 from bs4 import BeautifulSoup
-from scipy import stats #The SciPy stats module
+from scipy import stats  # The SciPy stats module
 from pandas_datareader.famafrench import get_available_datasets
 import pandas_datareader.data as web
+
 
 def get_european_weights_ken_french():
     """
@@ -22,30 +23,39 @@ def get_european_weights_ken_french():
 
     """
 
+    START_DATE = "2000-05-01"
+    END_DATE = "2022-02-22"
 
-
-
-    START_DATE = '2000-05-01'
-    END_DATE = '2022-02-22'
-
-    ff_dict = web.DataReader('Europe_5_Factors', 'famafrench', start=START_DATE, end = END_DATE) # default is monthly  #ff_dict.keys()  #print(ff_dict["DESCR"])
-    factor_df = ff_dict[0]#monthly factors. 1 for annual
-    factor_df.rename(columns = {"Mkt-RF": "mkt_rf", "SMB":"smb", "HML":"hml", "RMW":"rmw", "CMA":"cma", "RF":"rf"}, inplace= True) 
+    ff_dict = web.DataReader(
+        "Europe_5_Factors", "famafrench", start=START_DATE, end=END_DATE
+    )  # default is monthly  #ff_dict.keys()  #print(ff_dict["DESCR"])
+    factor_df = ff_dict[0]  # monthly factors. 1 for annual
+    factor_df.rename(
+        columns={
+            "Mkt-RF": "mkt_rf",
+            "SMB": "smb",
+            "HML": "hml",
+            "RMW": "rmw",
+            "CMA": "cma",
+            "RF": "rf",
+        },
+        inplace=True,
+    )
     factor_df["mkt"] = factor_df.mkt_rf + factor_df.rf
-    factor_df = factor_df.apply(pd.to_numeric, errors='coerce').div(100)
-    #meandf = factor_df[["smb", "hml", "rmw", "cma"]].mean()*100
-    adjust_weights = factor_df[["smb", "hml", "rmw", "cma"]].mean()*100
-    return(adjust_weights.values)
+    factor_df = factor_df.apply(pd.to_numeric, errors="coerce").div(100)
+    # meandf = factor_df[["smb", "hml", "rmw", "cma"]].mean()*100
+    adjust_weights = factor_df[["smb", "hml", "rmw", "cma"]].mean() * 100
+    return adjust_weights.values
 
 
 def calculate_precentiles(rv_dataframe, metric_dict):
     """
     Function to calculate the percentiles of the calculated and obtained metrics. This gives the user and intuition about an particular stock compared to other stocks.
-    
+
     Inputs:
         - rv_dataframe(pd.DataFrame): A pd.DataFrame with the stockinformation.
         - metric_dict(dict): A python dicionary with the pairs ondicating for which metrics the percentiles should be calculated.
-    
+
     Returns:
         - A pd.DataFrame with the calculated percentiles and the initial info.
 
@@ -59,8 +69,9 @@ def calculate_precentiles(rv_dataframe, metric_dict):
     for row in rv_dataframe.index:
         value_percentiles = []
         for i, metric in enumerate(FF_metrics.keys()):
-            value_percentiles.append(rv_dataframe.loc[row, FF_metrics[metric]] * ff_weights[i])
-        rv_dataframe.loc[row, 'RV Score'] = sum(value_percentiles) / sum(ff_weights)
-        
-    return rv_dataframe
+            value_percentiles.append(
+                rv_dataframe.loc[row, FF_metrics[metric]] * ff_weights[i]
+            )
+        rv_dataframe.loc[row, "RV Score"] = sum(value_percentiles) / sum(ff_weights)
 
+    return rv_dataframe
--- src/final/task_create_product.py
+++ src/final/task_create_product.py
@@ -7,6 +7,7 @@ I tried to limit the cleaning in this step as much as possible since there is mo
 import itertools
 import numpy as np
 import pytask
+import time
 
 from src.config import BLD
 from src.config import SRC
@@ -18,7 +19,7 @@ def save_data(sample, path):
 
     """
     Function to save the data / sample as pickle in a specified location.
-    
+
     Inputs:
         sample(pd.DataFrame): DataFrame with the data that has to be saved as pickle.
         path (str): path of the file for which to perform the validation.
@@ -30,12 +31,13 @@ def save_data(sample, path):
 
 today = datetime.today().strftime("%Y-%m-%d")
 
+
 @pytask.mark.depends_on(SRC / "final" / f"proc_eurostoxx600_{today}.pkl")
 @pytask.mark.produces(SRC / "product_data" / f"f_proc_eurostoxx600_{today}.pkl")
 def task_create_product(depends_on, produces):
     """
     Pytask function to create the european product. The output can be found in src.product_data.
-    
+
     """
 
     rv_dataframe = pd.read_pickle(depends_on)
@@ -44,11 +46,11 @@ def task_create_product(depends_on, produces):
     except:
         pass
     metric_dict = {
-            'marketCap': 'MC_percentile',
-            'priceToBook':'PB_percentile',
-            'FF_Quality_mean' : 'FFQ(inv)_m_percentile',
-            'FF_Assets_Growth_mean': 'FFA_m_percentile',
-        }
+        "marketCap": "MC_percentile",
+        "priceToBook": "PB_percentile",
+        "FF_Quality_mean": "FFQ(inv)_m_percentile",
+        "FF_Assets_Growth_mean": "FFA_m_percentile",
+    }
 
     rv_dataframe_per = calculate_precentiles(rv_dataframe, metric_dict)
     # rv_dataframe_revGPconstraint = rv_dataframe_per[
@@ -57,7 +59,7 @@ def task_create_product(depends_on, produces):
     #     & (rv_dataframe_per.returnOnEquity >= 0)
     #     & (rv_dataframe_per.OpIncomeGrowth >= 0.05)
     # ]
-    #len(rv_dataframe_revGPconstraint)
+    # len(rv_dataframe_revGPconstraint)
 
     sorted_rc_frame = rv_dataframe_per.sort_values(by="RV Score").copy()
     sorted_rc_frame = sorted_rc_frame
@@ -84,37 +86,36 @@ def task_create_product(depends_on, produces):
     save_data(sorted_rc_frame, produces)
 
 
-
-
 if __name__ == "__main__":
-    
+
     today = datetime.today().strftime("%Y-%m-%d")
-    data_ls = [f"proc_{today}_val2_euro600.pkl",
-               f"proc_{today}_val2_de.pkl",
-               f"proc_{today}_val2_nyse.pkl",
-               f"proc_{today}_val2_nasdaq.pkl",
-               f"proc_{today}_val2_nikkei225.pkl",
-               f"proc_{today}_val2_amex.pkl"
-               ]
+    data_ls = [
+        f"proc_{today}_val2_euro600.pkl",
+        f"proc_{today}_val2_de.pkl",
+        f"proc_{today}_val2_nyse.pkl",
+        f"proc_{today}_val2_nasdaq.pkl",
+        f"proc_{today}_val2_nikkei225.pkl",
+        f"proc_{today}_val2_amex.pkl",
+    ]
     for datalist in data_ls:
 
-        rv_dataframe = pd.read_pickle( SRC / "final" / "processed_data" / datalist) 
+        rv_dataframe = pd.read_pickle(SRC / "final" / "processed_data" / datalist)
         try:
             rv_dataframe.drop("Unnamed: 0", axis=1, inplace=True)
         except:
             pass
-        
+
         metric_dict = {
-            'marketCap': 'MC_percentile',
-            'priceToBook':'PB_percentile',
-            'FF_Quality_mean' : 'FFQ(inv)_m_percentile',
+            "marketCap": "MC_percentile",
+            "priceToBook": "PB_percentile",
+            "FF_Quality_mean": "FFQ(inv)_m_percentile",
             #'FF_Quality_Growth' : 'FFQ(inv)_g_percentile',
-            'FF_Assets_Growth_mean': 'FFA_m_percentile',
+            "FF_Assets_Growth_mean": "FFA_m_percentile",
         }
         rv_dataframe_per = calculate_precentiles(rv_dataframe, metric_dict)
 
         sorted_rc_frame = rv_dataframe_per.sort_values(by="RV Score").copy()
         sorted_rc_frame.reset_index(drop=True, inplace=True)
-         
-        sorted_rc_frame.to_pickle(SRC / "final" / "processed_data" /f"f_{datalist}")
+
+        sorted_rc_frame.to_pickle(SRC / "final" / "processed_data" / f"f_{datalist}")
         time.sleep(1)
